52c068604a1cf4cd5704ace904114c34741f8af0
wonksknowsuchin <suchin.ravi@wonksknow.com>
2022-01-14 09:27:08 -0800

    third upload

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='yw_hello',
-    version='0.2',
+    version='0.3',
     license='MIT',
     author="Suchin Ravi",
     author_email='suchin.ravi@wonksknow.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages('yw_hello'),
+    package_dir={'': 'yw_hello'},
     url='https://github.com/wonksknowsuchin/yw_hello',
     keywords='Youngwonks Hello',
     install_requires=[],
